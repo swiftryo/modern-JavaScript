7996c9511e71fbd5b2d027048b075063e5762b14
ryo <68486607+swiftryo@users.noreply.github.com>
2021-02-06 20:44:31 +0900

    add Arrow function Template string

--- src/index.js
+++ src/index.js
@@ -49,3 +49,39 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+// テンプレート文字列
+
+// const name = "名前";
+// const age = 12;
+// // 私の名前は名前です。年齢は12歳です。
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+// アロー関数
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+
+// console.log(func1("func1です"));
+
+// アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
