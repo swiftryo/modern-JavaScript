be5aed88ae2ee286442a82c36439ad32a26560c7
ryo <68486607+swiftryo@users.noreply.github.com>
2021-02-07 17:57:53 +0900

    add Default value Spread syntax

--- src/index.js
+++ src/index.js
@@ -111,3 +111,40 @@
 // const [name, age] = myProfile;
 // const message4 = `私は${name}です。年齢は${age}だよ！`;
 // console.log(message4);
+
+// デフォルト値
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// sayHello("名前");
+
+// スプレット構文  ...
+
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
