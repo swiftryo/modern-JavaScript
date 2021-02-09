0cfbf6c41bbdd6d201a6bb858e7aadcd56ddc13d
ryo <68486607+swiftryo@users.noreply.github.com>
2021-02-09 15:10:03 +0900

    add delete

--- src/index.js
+++ src/index.js
@@ -24,7 +24,10 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    // 削除処理
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
   // divの子要素に各要素を設定
